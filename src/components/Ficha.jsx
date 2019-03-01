6e6cb50ed3f319e54061b062dd6270e22da0024f
Ralph Perdomo <ralph@prdmo.com>
2019-03-01 11:33:05 -0800

    add fichas builder to make game pieces

--- src/components/Ficha.jsx
+++ src/components/Ficha.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
+const buildFichaSet = (dots = 9) => {
+  let builtSet = [];
+
+  for (let bottom = dots; bottom >= 0; bottom--) {
+    for (let i = 0; i <= bottom; i++) {
+      builtSet.push([i, bottom]);
+    }
+  }
+
+  return builtSet;
+};
+
 function Ficha() {
   return <div>Ficha works!</div>;
 }
