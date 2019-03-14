01fd72e71c8c7de4b332f47b6ccc6b97fb2cce1c
Ralph Perdomo <ralph@prdmo.com>
2019-03-13 20:05:23 -0700

    deployed

--- src/components/helpers/buildFichaSet.js
+++
@@ -1,9 +0,0 @@
-export const buildFichaSet = (dots = 9) => {
-  let builtSet = [];
-  for (let bottom = dots; bottom >= 0; bottom--) {
-    for (let i = 0; i <= bottom; i++) {
-      builtSet.push([i, bottom]);
-    }
-  }
-  return builtSet;
-};
--- src/components/helpers/reconstructObject.js
+++
@@ -1,7 +0,0 @@
-export const reconstructObject = inputArray => {
-  const outputObject = {};
-  inputArray.forEach(ficha => {
-    outputObject[ficha.fichaId] = ficha;
-  });
-  return outputObject;
-};
