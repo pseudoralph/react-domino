9f24976b7e8cf1cdaa46943f142a8fcf89a22901
Ralph Perdomo <ralph@prdmo.com>
2019-05-12 13:10:36 -0700

    create components for mobile play and make selection screen

---
+++ src/components/Display.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Display = () => {
+  return <div>Display goes here</div>;
+};
+
+export default Display;
---
+++ src/components/MobileControl.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import Draggable from 'react-beautiful-dnd';
+
+const MobileControl = () => {
+  return <div>Mobile Control</div>;
+};
+
+export default MobileControl;
