516b544da96590cbf4e08c3870a6fd1061db76b4
Ralph Perdomo <ralph@prdmo.com>
2019-05-12 13:12:20 -0700

    add hashrouter to app

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './components/Game';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<Game />, document.getElementById('react-app-root'));
+ReactDOM.render(
+  <HashRouter>
+    <Game />
+  </HashRouter>,
+  document.getElementById('react-app-root')
+);
 
 /*eslint-disable */
 if (module.hot) {
