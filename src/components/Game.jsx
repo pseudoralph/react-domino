6d47affacc06e90c9cca873661ae9edccc60635d
Ralph Perdomo <ralph@prdmo.com>
2019-05-12 13:13:29 -0700

    add react-dom-router and components for classic/new gameplay

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,18 +3,26 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import GameLoader from './GameLoader';
+import Selection from './Selection';
+import MobileControl from './MobileControl';
+import Display from './Display';
+
 import rootReducer from '../reducers';
+import { Route, Switch } from 'react-router-dom';
 
 const combinedReducers = combineReducers(rootReducer);
 const store = createStore(combinedReducers, applyMiddleware(thunkMiddleware));
 
 function Game() {
   return (
-    <div>
-      <Provider store={store}>
-        <GameLoader />
-      </Provider>
-    </div>
+    <Provider store={store}>
+      <Switch>
+        <Route exact path="/" component={Selection} />
+        <Route path="/classic" component={GameLoader} />
+        <Route path="/controller" component={MobileControl} />
+        <Route path="/display" component={Display} />
+      </Switch>
+    </Provider>
   );
 }
 
