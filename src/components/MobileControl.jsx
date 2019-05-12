01576558f192d18294f754af19d7c5d83f330f18
Ralph Perdomo <ralph@prdmo.com>
2019-05-12 15:24:17 -0700

    save

--- src/components/MobileControl.jsx
+++ src/components/MobileControl.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Draggable from 'react-beautiful-dnd';
 
 const MobileControl = () => {
-  return <div>Mobile Control</div>;
+  const [isHosting, setIsHosting] = useState({
+    slectionMade: false,
+    hostStatus: null
+  });
+
+  const handleButtonClick = event => {
+    setIsHosting({
+      slectionMade: true,
+      hostStatus: event.target.id
+    });
+  };
+
+  return (
+    <div>
+      <button id="host" onClick={handleButtonClick}>
+        Host
+      </button>
+      <button id="join" onClick={handleButtonClick}>
+        Join
+      </button>
+      {isHosting.hostStatus ? isHosting.hostStatus : 'no slection'}
+    </div>
+  );
 };
 
 export default MobileControl;
