4b54757c626a460e62d373033ffe5bb9989d1634
Ralph Perdomo <ralph@prdmo.com>
2019-05-12 18:05:38 -0700

    add webpack style-loader and css-loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -37,6 +37,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
         test: /\.jsx?$/,
         enforce: 'pre',
         loader: 'eslint-loader',
