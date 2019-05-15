c8e11a4874f0bfbc073bef7b7b6a78eb048fa6e6
Ralph Perdomo <ralph@prdmo.com>
2019-05-14 17:47:33 -0700

    abstract out stylized box for controller and display options

--- src/components/Selection/index.js
+++ src/components/Selection/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ClassicMode from './classicMode';
 import MultiMode from './multiMode';
 
-const Selection = () => {
+const Selection = props => {
   const handleInfoVis = htmlRef => {
     htmlRef.current.style.visibility =
       htmlRef.current.style.visibility === 'hidden' ? 'visible' : 'hidden';
   };
 
+  const handleRedir = gameInfo => {
+    props.history.push('/controller', { something: true });
+  };
+
+  console.log(props);
   return (
     <div className="selection-wrapper">
       <h1>Select a game mode</h1>
       <div>
         <ClassicMode handleInfoVis={handleInfoVis} />
-        <MultiMode handleInfoVis={handleInfoVis} />
+        <MultiMode handleInfoVis={handleInfoVis} handleRedir={handleRedir} />
       </div>
     </div>
   );
--- src/components/Selection/multiMode.js
+++ src/components/Selection/multiMode.js
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import randomWords from 'random-words'; //eslint-disable-line no-unused-vars
+
 import { Link } from 'react-router-dom';
 import info from '../../assets/icons/info.svg';
 import '../../styles/selection.css';
 
-const MultiMode = ({ handleInfoVis }) => {
+const MultiMode = props => {
+  const styling = {
+    box: {
+      position: 'relative',
+      boxShadow: 'rgba(64, 78, 67, 0.55) 1px 1px 4px 0px',
+      borderRadius: '0.5em',
+      background: 'rgba(152, 179, 152, 0.9)',
+      marginBottom: '1em',
+      display: 'inline-block',
+      width: '100%',
+      padding: '2em',
+      boxSizing: 'border-box',
+      textAlign: 'center',
+      maxWidth: '30em'
+    }
+  };
+
+  const [hostedGame, setHostedGame] = useState({
+    gameId: null,
+    player: null
+  });
+
+  const handleHostedGame = () => {
+    setHostedGame({
+      gameId: randomWords(2).join('-'),
+      player: 'p1'
+    });
+
+    props.handleRedir();
+
+    // this.dispatch(startGame(gameId));
+    // this.dispatch(grabFichas(gameId, 'p1'));
+  };
+
   return (
-    <div className="selection-option">
-      <div>
+    <div className="selection-box">
+      <div style={styling.box}>
+        <button onClick={handleHostedGame}>Host</button>
         <p>
-          <Link to="/controller">Controller</Link>
+          <Link
+            to={{
+              pathname: '/controller',
+              state: { hosted: true }
+            }}
+          >
+            HOST
+          </Link>
         </p>
       </div>
-      <div>
+      <div style={styling.box}>
         <p>
           <Link to="/display">Display</Link>
         </p>
