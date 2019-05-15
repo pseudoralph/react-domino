c1561ef307ba0ad35f5e6dd62061344e62d1290d
Ralph Perdomo <ralph@prdmo.com>
2019-05-14 17:46:58 -0700

    toggling between browserrouter and hashrouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './components/Game';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <HashRouter>
