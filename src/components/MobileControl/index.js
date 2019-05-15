b512d0a41e09e272e5384f466efebe1ca5a0a3eb
Ralph Perdomo <ralph@prdmo.com>
2019-05-14 20:51:00 -0700

    toggling between class and functional component

--- src/components/MobileControl/index.js
+++ src/components/MobileControl/index.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { watchHand, watchGame } from '../../actions';
 // import Draggable from 'react-beautiful-dnd';
 
+// class MobileControl extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   componentWillMount() {
+//     console.log('ok!');
+
+//     this.props.dispatch(
+//       watchHand(
+//         this.props.location.state.gameId,
+//         this.props.location.state.player
+//       )
+//     );
+//     this.props.dispatch(watchGame(this.props.location.state.gameId));
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.props.location.state
+//           ? `game id: ${this.props.location.state.gameId}`
+//           : 'you made it here alone'}
+//       </div>
+//     );
+//   }
+// }
+
 const MobileControl = props => {
+  useEffect(() => {
+    props.dispatch(
+      watchHand(props.location.state.gameId, props.location.state.player)
+    );
+    props.dispatch(watchGame(props.location.state.gameId));
+    console.log('ok');
+  }, []);
+
   return (
     <div>
       {props.location.state
@@ -16,4 +55,4 @@ MobileControl.propTypes = {
   location: PropTypes.object
 };
 
-export default MobileControl;
+export default connect()(MobileControl);
