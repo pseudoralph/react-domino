c45db989e9fae06029bdc22062a9368d86303ff5
Ralph Perdomo <ralph@prdmo.com>
2019-05-18 13:08:01 -0700

    duplicate ficha and specialize for touch

---
+++ src/components/FichaTouch.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Face from './Face';
+import { STYLES } from '../assets/styling';
+// import '../styles/ficha.css';
+
+import { ItemTypes } from '../constants/itemTypes';
+import { DragSource } from 'react-dnd';
+
+const fichaSource = {
+  beginDrag(props) {
+    console.log(props);
+    return {};
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  };
+}
+
+function handleDrag(event, ficha) {
+  event.dataTransfer.setData('ficha', JSON.stringify(ficha));
+}
+
+function Ficha(props) {
+  const {
+    value,
+    fichaId,
+    player,
+    gameId,
+    fichaStyling,
+    connectDragSource,
+    isDragging
+  } = props;
+  return connectDragSource(
+    <div
+      style={{
+        ...STYLES.activePlayersHand.ficha,
+        ...STYLES.fichaStyling[fichaStyling],
+        bottom: 0
+      }}
+      className="ficha"
+    >
+      <Face value={value[0]} />
+      <hr style={STYLES.activePlayersHand.line} />
+      <Face value={value[1]} />
+    </div>
+  );
+}
+
+Ficha.propTypes = {
+  value: PropTypes.array.isRequired,
+  fichaId: PropTypes.string.isRequired,
+  player: PropTypes.string,
+  gameId: PropTypes.string,
+  renderPos: PropTypes.number,
+  fichaStyling: PropTypes.string
+};
+
+export default DragSource(ItemTypes.FICHA, fichaSource, collect)(Ficha);
