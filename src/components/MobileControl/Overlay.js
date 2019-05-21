da3e12fbf7445c4e2e8f9094ec6e4134e389c56a
Ralph Perdomo <ralph@prdmo.com>
2019-05-21 15:32:32 -0700

    abstract out HOC for ficha styling

---
+++ src/components/MobileControl/Overlay.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Overlay = ({ children, placement }) => {
+  const definedMargin = '2.5rem';
+  const marginOffset =
+    placement === 'left'
+      ? { marginRight: definedMargin }
+      : placement === 'right'
+      ? { marginLeft: definedMargin }
+      : null;
+  const gradientOverlay =
+    placement === 'left'
+      ? {
+          backgroundImage: 'linear-gradient(180deg, #ffffff00 , #8db38d)',
+          backgroundColor: 'unset'
+        }
+      : placement === 'right'
+      ? {
+          backgroundImage: 'linear-gradient(180deg, #8db38d, #ffffff00)',
+          backgroundColor: 'unset'
+        }
+      : null;
+  return (
+    <div
+      style={{
+        transform: 'rotate(-90deg)',
+        display: 'inline-block',
+        position: 'relative',
+        top: '-.35rem',
+        ...marginOffset
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: 'rgba(255, 255, 255, 0.25)',
+          zIndex: 1,
+          borderRadius: '0.33em',
+          ...gradientOverlay
+        }}
+      >
+        {React.cloneElement(children, {
+          transform: { zIndex: -1 }
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Overlay;
