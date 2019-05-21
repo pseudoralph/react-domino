1edcb3d1e762166d1ffdb52e32cc8e171a1d33ad
Ralph Perdomo <ralph@prdmo.com>
2019-05-20 22:05:50 -0700

    add components and logic for 1 and 2 ficha previews

--- src/components/MobileControl/dropZoneContainer.js
+++ src/components/MobileControl/dropZoneContainer.js
@@ -4,16 +4,46 @@ import PropTypes from 'prop-types';
 import FichaDisplay from './FichaDisplay';
 
 import { getLeftMostFicha, getRightMostFicha } from '../../helpers/matchers';
+import SpinningFicha from '../../assets/SpinningFicha';
 
-const Overlay = ({ children }) => {
+const Overlay = ({ children, placement }) => {
+  const definedMargin = '2.5rem';
+  const marginOffset =
+    placement === 'left'
+      ? { marginRight: definedMargin }
+      : placement === 'right'
+      ? { marginLeft: definedMargin }
+      : null;
+  const gradientOverlay =
+    placement === 'left'
+      ? {
+          backgroundImage: 'linear-gradient(180deg, #ffffff00 , #8db38d)',
+          backgroundColor: 'unset'
+        }
+      : placement === 'right'
+      ? {
+          backgroundImage: 'linear-gradient(180deg, #8db38d, #ffffff00)',
+          backgroundColor: 'unset'
+        }
+      : null;
   return (
     <div
       style={{
         transform: 'rotate(-90deg)',
-        display: 'inline-block'
+        display: 'inline-block',
+        position: 'relative',
+        top: '-.65rem',
+        ...marginOffset
       }}
     >
-      <div style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)', zIndex: 1 }}>
+      <div
+        style={{
+          backgroundColor: 'rgba(255, 255, 255, 0.25)',
+          zIndex: 1,
+          borderRadius: '0.33em',
+          ...gradientOverlay
+        }}
+      >
         {React.cloneElement(children, {
           transform: { zIndex: -1 }
         })}
@@ -23,25 +53,27 @@ const Overlay = ({ children }) => {
 };
 
 const DropZoneContainer = ({ dispatch, fichasInPlay, isActivePlayer }) => {
-  console.log(fichasInPlay); //eslint-disable-line no-console
   let leftMost, rightMost, only;
 
   if (!Object.keys(fichasInPlay).length) {
     return (
       <div className="drop-zone-container-wrapper">
-        <div className="dragzone-container-waiting">
-          {isActivePlayer ? (
+        {isActivePlayer ? (
+          <div>
             <DropZone
               dispatch={dispatch}
               side={'left'}
               dropStyling={'dropzone'}
             >
-              <p>Make the first move</p>
+              <SpinningFicha />
             </DropZone>
-          ) : (
-            'Waiting on opponent to move'
-          )}
-        </div>
+            <p>Place your first piece</p>
+          </div>
+        ) : (
+          <div className="dragzone-container-waiting">
+            Waiting on player 1 to move...
+          </div>
+        )}
       </div>
     );
   } else if (Object.keys(fichasInPlay).length === 1) {
@@ -49,19 +81,21 @@ const DropZoneContainer = ({ dispatch, fichasInPlay, isActivePlayer }) => {
 
     return (
       <div className="drop-zone-container-wrapper">
-        <DropZone
-          dispatch={dispatch}
-          side="left"
-          dropStyling={'dropzone dropzone-left'}
-        />
-        <Overlay>
-          <FichaDisplay value={only.value} />
-        </Overlay>
-        <DropZone
-          dispatch={dispatch}
-          side="right"
-          dropStyling={'dropzone dropzone-right'}
-        />
+        <div className="drop-zone-container-with-fichas">
+          <DropZone
+            dispatch={dispatch}
+            side="left"
+            dropStyling={'dropzone dropzone-left'}
+          />
+          <Overlay>
+            <FichaDisplay fichaStyling={'dropZoneView'} value={only.value} />
+          </Overlay>
+          <DropZone
+            dispatch={dispatch}
+            side="right"
+            dropStyling={'dropzone dropzone-right'}
+          />
+        </div>
       </div>
     );
   } else if (Object.keys(fichasInPlay).length > 1) {
@@ -70,19 +104,28 @@ const DropZoneContainer = ({ dispatch, fichasInPlay, isActivePlayer }) => {
 
     return (
       <div className="drop-zone-container-wrapper">
-        <h3>drag here</h3>
-        <div>
+        <div className="drop-zone-container-with-fichas">
           <DropZone
             dispatch={dispatch}
             side="left"
-            assistFicha={leftMost}
-            transform={{ transform: 'rotate(-90deg)' }}
+            dropStyling={'dropzone dropzone-left'}
           />
+          <Overlay placement={'left'}>
+            <FichaDisplay
+              fichaStyling={'dropZoneView'}
+              value={leftMost.value}
+            />
+          </Overlay>
+          <Overlay placement={'right'}>
+            <FichaDisplay
+              fichaStyling={'dropZoneView'}
+              value={rightMost.value}
+            />
+          </Overlay>
           <DropZone
             dispatch={dispatch}
             side="right"
-            assistFicha={rightMost}
-            transform={{ transform: 'rotate(-90deg)' }}
+            dropStyling={'dropzone dropzone-right'}
           />
         </div>
       </div>
