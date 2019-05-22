9ffe797e2f6a458103197345eb2e42b0ab16e2b9
Ralph Perdomo <ralph@prdmo.com>
2019-05-22 15:02:09 -0700

    add additional render helper for display board with 38 avail plays

--- src/helpers/fichaRenderHelper.js
+++ src/helpers/fichaRenderHelper.js
@@ -15,3 +15,27 @@ export const fichaRenderHelper = pos => {
     return fichaStyling[0];
   }
 };
+
+export const fichaRenderHelperDisplay = pos => {
+  const fichaStyling = [
+    'fichaDisplayLeft',
+    'fichaDisplayRight',
+    'fichaDisplayMinus90',
+    'fichaDisplayPlus90'
+  ];
+  if ((pos >= -11 && pos <= -5) || (pos >= 5 && pos <= 11)) {
+    return fichaStyling[3];
+  } else if (
+    (pos >= -19 && pos <= -13) ||
+    (pos >= -3 && pos <= 3) ||
+    (pos >= 13 && pos <= 19)
+  ) {
+    return fichaStyling[2];
+  } else if (pos == -12 || pos == 4 || pos == 19) {
+    //right
+    return fichaStyling[1];
+  } else if (pos == -4 || pos == 12) {
+    //left
+    return fichaStyling[0];
+  }
+};
