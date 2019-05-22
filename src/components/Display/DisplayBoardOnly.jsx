e060d837a9d2673c6aae6a76d9b0bcd4f9885fb3
Ralph Perdomo <ralph@prdmo.com>
2019-05-22 15:06:58 -0700

    change ficha render component to specialised one

--- src/components/Display/DisplayBoardOnly.jsx
+++ src/components/Display/DisplayBoardOnly.jsx
@@ -1,52 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import { STYLES } from '../../assets/styling';
-
-import Ficha from '../Ficha';
-import boardRenderHelper from '../../helpers/boardRenderHelper';
+import FichaDisplay from './FichaDisplayBoard';
+import { boardRenderHelperDisplay } from '../../helpers/boardRenderHelper';
+import { fichaRenderHelperDisplay } from '../../helpers/fichaRenderHelper';
+import { STYLES } from './styling';
 import c from '../../constants';
 
-class PlayBoard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const PlayBoard = ({ fichas }) => {
+  const { fichasGridDisplay } = c;
 
-  render() {
-    const { fichas, gameId } = this.props;
+  const renderedOutput = Object.keys(fichas).length
+    ? boardRenderHelperDisplay(fichas)
+    : Array(40).fill(null);
 
-    const renderedOutput = Object.keys(fichas).length
-      ? boardRenderHelper(fichas)
-      : Array(40).fill(null);
-
-    return (
-      <div style={STYLES.board} className="board">
-        <div style={STYLES.board.playable}>
-          {renderedOutput.map((ficha, i) =>
-            ficha ? (
-              <div key={i} id={c.fichasGrid[i]} className="path">
-                <Ficha
-                  value={ficha.value}
-                  fichaId={ficha.fichaId}
-                  renderPos={ficha.renderPos}
-                  fichaStyling={ficha.fichaStyling}
-                />
-              </div>
-            ) : (
-              <div
-                key={i}
-                id={c.fichasGrid[i]}
-                className={c.fichasGrid[i] ? 'path' : null}
-              >
-                &nbsp;
-              </div>
-            )
-          )}
-        </div>
+  return (
+    <div style={STYLES.board} className="board">
+      <div style={STYLES.board.playable}>
+        {renderedOutput.map((ficha, i) =>
+          ficha ? (
+            <div
+              key={i}
+              id={fichasGridDisplay[i]}
+              className="path"
+              style={STYLES.board.path}
+            >
+              <FichaDisplay
+                value={ficha.value}
+                fichaStyling={fichaRenderHelperDisplay(ficha.renderPos)}
+              />
+            </div>
+          ) : (
+            <div
+              key={i}
+              id={fichasGridDisplay[i]}
+              className={fichasGridDisplay[i] ? 'path' : null}
+            >
+              &nbsp;
+            </div>
+          )
+        )}
       </div>
-    );
-  }
-}
+      <div style={STYLES.board.status} className="status" />
+    </div>
+  );
+};
 
 PlayBoard.propTypes = {
   gameId: PropTypes.string.isRequired,
