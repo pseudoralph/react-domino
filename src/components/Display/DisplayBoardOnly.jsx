9a7a91ef9d5a0816b6ff68a2506f632639c160c2
Ralph Perdomo <ralph@prdmo.com>
2019-05-22 17:42:09 -0700

    add status bar with active player and game code

--- src/components/Display/DisplayBoardOnly.jsx
+++ src/components/Display/DisplayBoardOnly.jsx
@@ -5,8 +5,10 @@ import { boardRenderHelperDisplay } from '../../helpers/boardRenderHelper';
 import { fichaRenderHelperDisplay } from '../../helpers/fichaRenderHelper';
 import { STYLES } from './styling';
 import c from '../../constants';
+import domino from '../../assets/icons/domino.svg';
 
-const PlayBoard = ({ fichas }) => {
+const PlayBoard = ({ fichas, gameStatus, gameId }) => {
+  const { activePlayer } = gameStatus;
   const { fichasGridDisplay } = c;
 
   const renderedOutput = Object.keys(fichas).length
@@ -40,14 +42,38 @@ const PlayBoard = ({ fichas }) => {
           )
         )}
       </div>
-      <div style={STYLES.board.status} className="status" />
+      <div style={STYLES.board.status} className="status">
+        <div style={STYLES.board.status.playerInfo}>
+          {activePlayer === 'p1' && (
+            <img src={domino} alt="ficha" style={STYLES.board.status.icon} />
+          )}
+          p1
+        </div>
+        <div style={STYLES.board.status.playerInfo}>
+          {activePlayer === 'p2' && (
+            <img
+              src={domino}
+              alt="ficha"
+              style={{
+                ...STYLES.board.status.icon,
+                top: '0.33rem',
+                left: '8.75rem'
+              }}
+            />
+          )}
+          p2
+        </div>
+
+        <div style={STYLES.board.status.gameInfo}>{gameId}</div>
+      </div>
     </div>
   );
 };
 
 PlayBoard.propTypes = {
   gameId: PropTypes.string.isRequired,
-  fichas: PropTypes.object
+  fichas: PropTypes.object,
+  gameStatus: PropTypes.object
 };
 
 export default PlayBoard;
